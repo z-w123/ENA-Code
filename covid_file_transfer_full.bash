668bbeed45577772266e10bd8a4c7b1bed8b2ce7
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-01-05 10:43:50 +0000

    Update covid_file_transfer_full.bash

--- covid_file_transfer_full.bash
+++ covid_file_transfer_full.bash
@@ -12,14 +12,13 @@ covid-util list
 
 echo "transferring files to the following ebi-cli directory: /nfs/production/ena_utils/covid/${UUID}"
 
-#logs in to correct dir on ebi-cli + activate aws + creates UUID dir on ebi-cli & then copy files from covid-utils upload area to ebi-cli location 
+#logs in to correct dir on ebi-cli + activate aws + creates UUID dir on ebi-cli & then copy files from covid-utils upload area to ebi-cli location and closes login shell
 ssh -t ${USERNAME}@ebi-cli "cd /nfs/production/ena_utils/covid; cd venv/; source bin/activate; aws configure; cd bin; echo && echo 'first creating directory /nfs/production/ena_utils/covid/${UUID} on ebi-cli' && echo; mkdir /nfs/production/ena_utils/covid/${UUID}; cd /nfs/production/ena_utils/covid/${UUID}; aws --endpoint-url https://s3.embassy.ebi.ac.uk s3 cp s3://covid-utils-ui-88560523/${UUID} . --recursive; echo && echo 'files transferred to ebi-cli:' && echo; ls -lthra; exit; bash -l"
-#not sure yet how to automate user input for aws?
-#if we add more commands to this script, will they run in the ssh terminal or a normal one? No commands will not run, expects user typed input only
+#note: cannot add more commands to the line above and leave the ssh terminal open, as the login shell expects user typed input only
 
 #part 2: copies files from ebi-cli dir back to local machine
 
-mkdir submission_files_${UUID} #creates drag n drop submission directory on local machine (which files incl. spreadsheet will be transferred to)
+mkdir submission_files_${UUID} #creates drag n drop submission directory on local machine (which files will be transferred to)
 cd submission_files_${UUID}
 
 { echo "cd /nfs/production/ena_utils/covid/${UUID}" && echo "ls -lthra" && echo "lpwd" && echo "mget *" && echo "bye" ; } | sftp ${USERNAME}@ebi-cli  #copies all files from ebi-cli to local dir; run from local dir
@@ -32,4 +31,4 @@ cd submission_files_${UUID}
 
 #2) automate aws user input? 
 #	option 1: pipe the contents of text file with credentials:  cat "aws_credentials.txt" | ./part_2_covid_file_transfer.bash, but gives error: Pseudo-terminal will not be allocated because stdin is not a terminal.
-#	option 2 : write an expect script cont. credentials and then call that from this bash script (but this will probably only work on ubuntu- and is tricky)
+#	option 2 : write an expect script cont. credentials and then call that from this bash script (but this does not seem to work on git bash)
