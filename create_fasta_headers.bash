e4998e04e0eccfd09a0d7cfb3cb2abbc3056319a
z-w123 <45365618+z-w123@users.noreply.github.com>
2021-04-05 16:10:17 +0100

    Add files via upload

---
+++ create_fasta_headers.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+#script to extract all fasta headers and append to text file
+
+printf "all fasta files in this directory are:"
+echo 
+ls *fasta.gz 
+
+FASTAS_GZ=`ls *fasta.gz`
+
+for fasta in $FASTAS_GZ; do
+	gunzip $fasta 
+done
+
+UNZ_FASTA=`ls *.fasta`
+
+for unzfasta in $UNZ_FASTA; do
+	head -1 $unzfasta | tr -d '>' >> fasta_headers.txt 
+done
+
+gzip *.fasta
+
+echo `cat fasta_headers.txt | wc -l` "fasta headers"
+
+if [[ $(sort fasta_headers.txt | uniq -c -d) ]]; then #if uniq produces some output then:
+	echo "ERROR: Some fastas have duplicate headings. Submitter should ensure headings for each fasta are unique"
+	echo
+	echo "duplicate headers below"	
+	sort fasta_headers.txt | uniq -c -d
+else
+	: #do nothing
+fi
+
+
+
