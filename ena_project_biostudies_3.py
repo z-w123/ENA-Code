52d8e181ea10f18a5db2a60631dc60056c58e3c5
Zahra Waheed <zahra@ebi.ac.uk>
2021-05-18 13:53:28 +0100

    add functionality to link multiple projects

--- ena_project_biostudies_3.py
+++ ena_project_biostudies_3.py
@@ -1,4 +1,5 @@
 ## retrieving project xml data (for single project only)
+####TODO: accept project as an arguement#####
 import requests, xmltodict
 url_start = "https://www.ebi.ac.uk/ena/browser/api/xml"
 project_acc = "PRJEB39337"
@@ -27,24 +28,20 @@ project_accession = (data['PROJECT_SET']['PROJECT']['@accession'])
 
 ## creating two 'key:value' style lists
 # projects = ["PRJEB38723"]
-keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", "", "Link", "Description", "Type", "", "Link", "Type", "", "Author", "Name", "Email", "<affiliation>", "", "Organization", "Name"]
+#keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", "", "Link", "Description", "Type", "", "Link", "Type", "", "Author", "Name", "Email", "<affiliation>", "", "Organization", "Name"]
+keys = ["Submission", "Title", "ReleaseDate", "", "Study", "Title", "Description", "Center Name", "", "Link", "Description", "Type", ""]
 ###### TODO:I don't think the keys [] above accounts for multiple authors :/
 ###### TODO: have the script allow multiple projects to be specified#############
 values = []
 values.extend((None, project_name, release_date, ""))
 values.extend((None, project_title, project_description, center_name, ""))
 values.extend((project_accession, "Raw Data", "ENA", ""))
-values.extend(("<insert DOI here>", "DOI", "")) #will need manual adding
 
-
-##take in author details
-# e.g bob, dylan, jameela
-# bob@ebi.ac.uk, o2, EBI
-# dylan@ebi.ac.uk, o3, NCBI
-# jameela@uit.ac.uk , o4, uit
 
 import argparse
 parser = argparse.ArgumentParser()
+parser.add_argument('-ap', '--additional_projects', help='Additional ENA project accessions PRJEB#### to link to created Biostudies entry', type=str, nargs='*',
+                    required=True)  # additional projects
 parser.add_argument('-a', '--author', help='name of author/s separated by a space', type=str, nargs='*',
                     required=True)  # takes in multiple authors
 parser.add_argument('-e', '--email', help='email addresses separated by a space', type=str, nargs='*',
@@ -55,21 +52,55 @@ args = parser.parse_args()
 
 # print(args.author[1]) # how to specify which specific author or email address
 
-authors = len(args.author)
+additional_projects = len(args.additional_projects)
 
+def link_additional_projects(args):
+    for a_project in range(additional_projects):
+        keys.extend(("Link", "Description", "Type", ""))
+        values.extend((args.additional_projects[a_project], "Raw Data", "ENA", ""))
+link_additional_projects(args)
+
+keys.extend(("Link", "Type", ""))
+values.extend(("<insert DOI here>", "DOI", "")) #will need manual adding
+
+
+##take in author details
+# e.g bob, dylan, jameela
+# bob@ebi.ac.uk, o2, EBI
+# dylan@ebi.ac.uk, o3, NCBI
+# jameela@uit.ac.uk , o4, uit
+
+
+
+
+
+
+authors = len(args.author)
+unique_institutions = len(set(args.institution))
 
 # print(vars(args)) #gets dictionary attribute form of the args?
 def create_author_entries(args):
     print()
     print("adding author details below:")
     for i in range(authors):
-        print()
-        print(args.author[i], args.email[i], "o" + str(i + 1))
-        print("o" + str(i + 1), args.institution[i])
-        values.extend(("None", args.author[i], args.email[i], "o" + str(i + 1), ""))  # to increment the affiliation
-        values.extend(("o" + str(i + 1), args.institution[i]))
+        keys.extend(("Author", "Name", "Email", "<affiliation>", ""))
+        values.extend(("", args.author[i], args.email[i]))
+        if authors == unique_institutions:
+            affiliation = "o" + str(i + 1)
+            values.extend((affiliation, "")) #value for author 'affiliation' key
+            keys.extend(("Organization", "Name"))
+            values.extend((affiliation, args.institution[i])) #but what if multiple authors are present?
 create_author_entries(args)
-# print(values)
+    ###TODO: add <affiliation> cell to each author block
+    ##check if args.institution contains duplicate values
+    ##if yes, use the length of unique_institutions (e.g, 3) and do str(u+1) for each institution and attribute it to the corresponding author
+    #organisation block#
+#    for u in range(unique_institutions):
+#        keys.extend(("Organization", "Name", ""))
+#        values.extend(("o" + str(u + 1), args.institution[u], "")) # this is breaking the script and giving a TypeError: list indices must be integers or slices, not str
+
+
+
 
 # def create_author_entries(args):
 #    for i in range(authors):
@@ -121,10 +152,10 @@ print("Keys and values are:", keys_values)
 import pandas as pd
 df = pd.DataFrame(data=keys_values)
 pd.set_option("display.max_rows", None, "display.max_columns", None)
-print()
-print(df)
+#print()
+#print(df)
 ## create the pagetab file - note csv format NOT accepted!
-df.to_excel('test5thMay.xlsx', header=None, index=False)
+df.to_excel('test18thMay.xlsx', header=None, index=False)
 
 
 # TODO2) enable multiple projects being linked to a single pagetab
